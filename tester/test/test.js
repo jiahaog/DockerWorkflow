b4dd0088ed51eb6feb5114d01b241640236c1c73
Jia Hao <jiahaog@gmail.com>
2015-10-06 13:10:02 +0800

    Add test container

---
+++ tester/test/test.js
@@ -0,0 +1,23 @@
+var request = require('request');
+
+describe('Can communicate with the web page', function () {
+    this.timeout(10000);
+    it('Should be able to make a request to the page', function () {
+        request('http://127.0.0.1:3000', function (error, response, body) {
+            if (error) {
+                done(error);
+                return;
+            }
+
+            done();
+        });
+    });
+});
+
+describe('Array', function(){
+    describe('indexOf()', function(){
+        it('should return -1 when the value is not present', function(){
+            assert.equal(-1, [1,2,3].indexOf(4));
+        });
+    });
+});
